fbb0441b18d75e0a99d4c12d244d02343f148251
Neo <neo.sud@gmail.com>
2025-02-19 22:17:16 -0800

    Logic
    - Changed card logic to reset XMult if money drops below 0
    - Added calculate() method so card works in-game (currently broken!)

--- Sheldon.lua
+++ Sheldon.lua
@@ -14,7 +14,8 @@ SMODS.Joker({
 		text = {
 			"When Blind is selected,",
 			"Sheldon steals {C:money}$#1#{}, but",
-			"gains {X:mult,C:white} X#1# {} Mult in return.",
+			"gains {X:mult,C:white} X#1# {} Mult in return",
+			"{s:0.8} Resets if money drops below {}{s:0.8,C:money}$0{}",
 			"{C:inactive}(Currently {X:mult,C:white} X#2# {C:inactive} Mult)",
 		},
 	},
@@ -32,5 +33,31 @@ SMODS.Joker({
 			card.ability.extra.initial,
 			card.ability.extra.curr_xmult,
 		} }
+	end,
+	calculate = function(self, card, context)
+		if context.setting_blind and not context.blueprint then
+			if G.GAME.dollars - card.ability.extra.initial > 0 then
+				card.ability.extra.curr_xmult = 1
+				return {
+					message = "No Gold!",
+					colour = C.G.RED,
+					dollars = G.GAME.dollars - card.ability.extra.initial,
+					card = card,
+				}
+			else
+				card.ability.extra.curr_xmult = card.ability.extra.curr_xmult + card.ability.extra.initial
+				return {
+					message = "Gimmee!",
+					colour = C.G.GREEN,
+					dollars = G.GAME.dollars - card.ability.extra.initial,
+					card = card,
+				}
+			end
+		end
+		if context.joker_main then
+			return {
+				xmult = card.ability.extra.curr_xmult,
+			}
+		end
 	end,
 })
